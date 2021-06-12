f26f0ba57bfa8ada7c5e728273976571ee14a2b9
Ariadne Conill <ariadne@dereferenced.org>
2021-06-12 17:49:00 -0600

    implement minimal REGISTER implementation

--- modules/m_register.c
+++ modules/m_register.c
@@ -1,6 +1,6 @@
 /*
- * modules/m_acc.c
- * Copyright (c) 2020 Ariadne Conill <ariadne@dereferenced.org>
+ * modules/m_register.c
+ * Copyright (c) 2020, 2021 Ariadne Conill <ariadne@dereferenced.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -44,48 +44,34 @@
 #include "propertyset.h"
 #include "account.h"
 
-static const char acc_desc[] = "Provides ACC command and ophion.dev/acc capability";
+static const char register_desc[] = "Provides REGISTER command and draft/account-registration capability";
 
-unsigned int CLICAP_ACC;
+unsigned int CLICAP_REGISTER;
 
-mapi_cap_list_av2 acc_cap_list[] = {
-	{ MAPI_CAP_CLIENT, "ophion.dev/acc", NULL, &CLICAP_ACC },
+mapi_cap_list_av2 register_cap_list[] = {
+	{ MAPI_CAP_CLIENT, "draft/account-registration", NULL, &CLICAP_REGISTER },
 	{ 0, NULL, NULL, NULL },
 };
 
-static void m_acc(struct MsgBuf *msgbuf_p, struct Client *client_p, struct Client *source_p, int parc, const char *parv[]);
+static void m_register(struct MsgBuf *msgbuf_p, struct Client *client_p, struct Client *source_p, int parc, const char *parv[]);
 
-struct Message acc_msgtab = {
-	"ACC", 0, 0, 0, 0,
-	{mg_unreg, {m_acc, 2}, mg_ignore, mg_ignore, mg_ignore, {m_acc, 2}}
+struct Message register_msgtab = {
+	"REGISTER", 0, 0, 0, 0,
+	{mg_unreg, {m_register, 2}, mg_ignore, mg_ignore, mg_ignore, {m_register, 2}}
 };
 
-mapi_clist_av1 acc_clist[] = {
-	&acc_msgtab, NULL
+mapi_clist_av1 register_clist[] = {
+	&register_msgtab, NULL
 };
 
 static int h_ircx_account_login;
 
-mapi_hlist_av1 acc_hlist[] = {
+mapi_hlist_av1 register_hlist[] = {
 	{ "ircx_account_login", &h_ircx_account_login },
 	{ NULL, NULL }
 };
 
-DECLARE_MODULE_AV2(acc, NULL, NULL, acc_clist, acc_hlist, NULL, acc_cap_list, NULL, acc_desc);
-
-struct acc_cmd {
-	const char *cmd;
-	void (*func)(struct Client *source_p, int parc, const char *parv[]);
-};
-
-static void m_acc_ls(struct Client *source_p, int parc, const char *parv[]);
-static void m_acc_register(struct Client *source_p, int parc, const char *parv[]);
-
-static struct acc_cmd acc_cmdlist[] = {
-	/* This list *MUST* be in alphabetical order */
-	{"LS", m_acc_ls},
-	{"REGISTER", m_acc_register},
-};
+DECLARE_MODULE_AV2(register, NULL, NULL, register_clist, register_hlist, NULL, register_cap_list, NULL, register_desc);
 
 static char saltChars[] = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	/* 0 .. 63, ascii - 64 */
@@ -147,64 +133,37 @@ make_sha512_salt(int length)
 }
 
 /*
- * ACC LS - list available properties of the authentication layer.
- * Parameters: takes no parameters
- */
-static void
-m_acc_ls(struct Client *source_p, int parc, const char *parv[])
-{
-	char acc_subcmd_buf[BUFSIZE] = {};
-	size_t i;
-
-	for (i = 0; i < ARRAY_SIZE(acc_cmdlist); i++)
-	{
-		rb_strlcat(acc_subcmd_buf, acc_cmdlist[i].cmd, sizeof acc_subcmd_buf);
-		rb_strlcat(acc_subcmd_buf, " ", sizeof acc_subcmd_buf);
-	}
-
-	sendto_one(source_p, ":%s ACC LS * SUBCOMMANDS :%s", me.name, acc_subcmd_buf);
-	sendto_one(source_p, ":%s ACC LS CREDTYPES :passphrase", me.name);
-}
-
-/*
- * ACC REGISTER - register an account with the authentication layer.
+ * REGISTER - register an account with the authentication layer.
  * Parameters:
- *   parv[1] = "REGISTER"
- *   parv[2] = account name
- *   parv[3] = callback URI (ignored)
- *   parv[4] = credential type (must be a recognized credential -- passphrase or certfp)
- *   parv[5] = credential (or unspecified for certfp)
+ *   parv[1] = account name or "*"
+ *   parv[2] = email or "*" (ignored)
+ *   parv[3] = credential
  */
 static void
-m_acc_register(struct Client *source_p, int parc, const char *parv[])
+m_register(struct MsgBuf *msgbuf_p, struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
 {
-	if (!irccmp(parv[4], "passphrase"))
-	{
-		sendto_one(source_p, ":%s FAIL ACC REG_INVALID_CRED_TYPE %s %s :Credential type is invalid",
-			   me.name, parv[2], parv[4]);
-		return;
-	}
-
 	bool new_account = false;
-	struct Account *account_p = account_find(parv[2], true, &new_account);
+	const char *accountname = *parv[1] == '*' ? client_p->name : parv[1];
+	struct Account *account_p = account_find(accountname, true, &new_account);
 
 	/* account already exists? */
 	if (account_p != NULL && !new_account)
 	{
-		sendto_one(source_p, ":%s FAIL ACC ACCOUNT_ALREADY_EXISTS %s :Account already exists",
-			   me.name, parv[2]);
+		sendto_one(source_p, ":%s FAIL REGISTER ACCOUNT_EXISTS %s :Account already exists",
+			   me.name, accountname);
 		return;
 	}
 
 	const char *salt = make_sha512_salt(16);
-	const char *crypted_passphrase = rb_crypt(parv[5], salt);
+	const char *crypted_passphrase = rb_crypt(parv[3], salt);
 
 	struct Property *prop = propertyset_add(&account_p->prop_list, "passphrase", crypted_passphrase, &me);
 
 	sendto_server(NULL, NULL, CAP_TS6, NOCAPS, ":%s TPROP account:%s %ld %ld %s :%s",
 		      use_id(&me), account_p->name, account_p->creation_ts, prop->set_at, prop->name, prop->value);
 
-	sendto_one_numeric(source_p, RPL_REG_SUCCESS, form_str(RPL_REG_SUCCESS), parv[2]);
+	sendto_one(source_p, ":%s REGISTER SUCCESS %s :Account created successfully",
+		   me.name, accountname);
 
 	hook_data_account_login req;
 
@@ -212,26 +171,4 @@ m_acc_register(struct Client *source_p, int parc, const char *parv[])
 	req.account_name = account_p->name;
 
 	call_hook(h_ircx_account_login, &req);
-}
-
-static int
-acc_cmd_search(const char *command, struct acc_cmd *entry)
-{
-	return irccmp(command, entry->cmd);
-}
-
-static void
-m_acc(struct MsgBuf *msgbuf_p, struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
-{
-	struct acc_cmd *cmd;
-
-	if(!(cmd = bsearch(parv[1], acc_cmdlist,
-				sizeof(acc_cmdlist) / sizeof(struct acc_cmd),
-				sizeof(struct acc_cmd), (void *) acc_cmd_search)))
-	{
-		sendto_one(source_p, ":%s FAIL ACC INVALID_ACC_SUBCMD :Invalid ACC subcommand", me.name);
-		return;
-	}
-
-	(cmd->func)(source_p, parc, parv);
 }
